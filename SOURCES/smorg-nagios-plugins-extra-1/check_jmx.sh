0b66fc54b0312f3c6b5a56c9ae0435cbd1af30d4
mclarkson <mclarkson@59809dea-28f1-416b-97ec-f5993ad77100>
2013-05-08 14:07:41 +0000

    Added check_jmx wrapper - formats perf data (Fix).

--- SOURCES/smorg-nagios-plugins-extra-1/check_jmx.sh
+++ SOURCES/smorg-nagios-plugins-extra-1/check_jmx.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
-OUTPUT=$(./check_jmx $@)
+OUTPUT=$(${0%/*}/check_jmx $@)
 EXIT_STATUS=$?
 VALUE="$(echo $OUTPUT | sed 's/.*{\(.*\)}.*/\1;/' | sed 's/;/; /g')"
 echo "$OUTPUT | $VALUE"
