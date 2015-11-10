62716fa844b26c2d1e0067616bd84b73d7be8697
Mark Clarkson <mark.clarkson@smorg.co.uk>
2015-11-10 13:56:18 +0000

    tar it up

--- jenkins_build.sh
+++ jenkins_build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 set -e
+pushd rpmbuild/SOURCES/
+tar cvzf smorg-nagios-plugins-extra-1.tar.gz smorg-nagios-plugins-extra-1
+popd
 cd rpmbuild
 rpmbuild --define "_topdir `pwd`" -bb SPECS/smorg-nagios-plugins-extra.spec
